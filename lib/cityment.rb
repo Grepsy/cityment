26d82f424fb06a2b7e4eebcb3965c94d571ec51e
Dirk Geurs <dirklectisch@mac.com>
2011-03-17 15:04:38 +0100

    + SourceDir lists date ranges of saved files

--- lib/cityment.rb
+++ lib/cityment.rb
@@ -1,6 +1,19 @@
 require 'cityment/api'
 require 'cityment/xml'
+require 'cityment/sourcedir'
 
 module Cityment
+  def fetch_sample
+    resp = API.fetch
+    XML::SourceDocument.parse resp.body
+  end
+  
+  def crawl_source
+    resp = API.fetch({:laterthan => '2011-01-01', :num => 200})
+    doc = XML::SourceDocument.parse resp.body
+
+  end
   
 end
+
+include Cityment
--- spec/unit/tc_sourcedir.rb
+++ spec/unit/tc_sourcedir.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require 'cityment/sourcedir'
+require 'date'
 
 describe SourceDir do
   describe :source_files do
@@ -8,9 +9,11 @@ describe SourceDir do
       assert(srcdir.source_files.include? "spec/fixtures/20110316-20110317.xml")
     end
   end
-  #describe :saved_dates do
-  #    srcdir = SourceDir.new
-  #    fixdates = [Date.parse('20110316'), Date.parse('20110317')]
-  #    assert(srcdir.saved_dates == fixdates)
-  #end
+  describe :saved_dates do
+    it "lists date ranges of saved files" do
+      srcdir = SourceDir.new 'spec/fixtures/'
+      fixdates = [Date.parse('20110316'), Date.parse('20110317')]
+      assert(srcdir.saved_dates[0] == fixdates, "Unexpected saved dates: #{srcdir.saved_dates}")
+    end
+  end
 end
