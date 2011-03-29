2adc7a898ef2850a1152ab84eacff964128046d7
Dirk Geurs <dirklectisch@mac.com>
2011-03-29 23:05:39 +0200

    Reworked the crawler using an Enumerator

--- lib/cityment.rb
+++ lib/cityment.rb
@@ -1,20 +1,35 @@
 require 'cityment/api'
 require 'cityment/xml'
 require 'cityment/xml/pox'
+require 'cityment/datetime'
 
 module Cityment
-  def fetch_sample
-    resp = API.fetch
+  
+  def parse_response resp
     XML::POX::SourceDocument.parse resp.body
   end
   
-  # def crawl_source maxage = '2011-01-01'
-  #   srcdir = SourceDir.new(XML::DIR + '/src')
-  #   srcdir.saved_dates
-  #   
-  #   resp = API.fetch({:laterthan => '2011-01-01', :num => 200})
-  #   doc = XML::SourceDocument.parse resp.body
-  # end
+  def crawl_range range = [DateTime.parse('2010-01-01'), DateTime.parse('2010-04-01')]
+    
+    save_dir = XML::POX::SourceDir.new "xml/src/#{range[0].stamp}-#{range[1].stamp}"
+    
+    crawler = Enumerator.new do |y|
+      while range[1] > range[0]
+        doc = parse_response(API.fetch_range(range[0], range[1]))
+        break if range[1] == doc.dates[0]
+        y << doc
+        range[1] = doc.dates[0]
+      end
+    end
+    
+    crawler.each do |doc|
+      save_dir.save(doc)
+      puts "Saved #{doc.dates.count} items, between #{range[0]} and #{range[1]}"  
+    end
+    
+    return save_dir
+    
+  end
   
 end
 
