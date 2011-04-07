4f38fdcdbde43fd6062827aa29c0e75764c13cdc
Dirk Geurs <dirklectisch@mac.com>
2011-04-07 22:24:58 +0200

    + Automatically parse response body

--- lib/cityment/api.rb
+++ lib/cityment/api.rb
@@ -1,17 +1,32 @@
 require 'rufus/verbs'
 require 'cityment/config'
 require 'cityment/datetime'
+require 'nokogiri'
 
 module Cityment
   module API
     
     ENDPOINT = Rufus::Verbs::EndPoint.new(Cityments::Config.load('endpoint'))
+    
+    PARSERS = {'application/xhtml+xml; charset=utf-8' => Nokogiri::XML::Document}
        
     def API.fetch params = {}
       def_params = ENDPOINT.opts[:params]
       req_params = def_params.merge params
       
       resp = ENDPOINT.get :params => req_params
+      resp = parse_body(resp) unless resp.class == Net::HTTP::Get
+      resp
+    end
+    
+    def API.parse_body response
+      content_type = response.header['content-type']
+      
+      if content_parser = PARSERS[content_type]
+        response.body = content_parser.send(:parse, response.body)
+      end
+      
+      response
     end
     
     def API.fetch_range date_range
