f45140afcc1e0f026e36bfd12ac79f379d8cf914
Dirk Geurs <dirklectisch@mac.com>
2011-04-10 20:05:02 +0200

    + complete_range method

--- lib/cityment/enumdaterange.rb
+++ lib/cityment/enumdaterange.rb
@@ -1,31 +1,41 @@
 require 'date'
 
-module EnumDateRange  
-  def each_year
-    years = []
-    if block_given?    
-      grouped_dates = self.group_by {|date| date.year}
-      grouped_dates.each_value do |dates|
-        years << (yield (dates[0]..dates[-1]))
-      end
-    else
-      return self.enum_for(:each_year)
+module Cityment
+  module EnumDateRange  
+    def EnumDateRange.complete_range
+      (Date.parse('2007-01-01')...Date.today).extend EnumDateRange
     end
-    years
-  end
-
-  def each_month
-    months = []
-    if block_given?
-      self.each_year do |range|
-        grouped_dates = range.group_by {|date| date.month}
+  
+    def each_year
+      years = []
+      if block_given?    
+        grouped_dates = self.group_by {|date| date.year}
         grouped_dates.each_value do |dates|
-          months << (yield (dates[0]..dates[-1]))
+          years << (yield (dates[0]..dates[-1]))
         end
+      else
+        return self.enum_for(:each_year)
       end
-    else
-      return self.enum_for(:each_month)
+      years
     end
-    months
-  end  
+
+    def each_month
+      months = []
+      if block_given?
+        self.each_year do |range|
+          grouped_dates = range.group_by {|date| date.month}
+          grouped_dates.each_value do |dates|
+            months << (yield (dates[0]..dates[-1]))
+          end
+        end
+      else
+        return self.enum_for(:each_month)
+      end
+      months
+    end
+  
+    def shorten last
+      return self
+    end  
+  end
 end
--- spec/unit/tc_enumdaterange.rb
+++ spec/unit/tc_enumdaterange.rb
@@ -4,6 +4,16 @@ require 'cityment/enumdaterange'
 include Cityment
 
 describe EnumDateRange do
+  describe :complete_range do
+    it 'returns a range between 2007-01-01 and today' do
+      range = EnumDateRange.complete_range
+      assert_equal(range.min, Date.parse('2007-01-01'))
+    end
+    it 'extends the range with EnumDateRange module' do
+      range = EnumDateRange.complete_range
+      assert(range.respond_to? :each_month)
+    end
+  end
   describe :each_year do
     it "it yields sub-ranges by year" do
       first = Date.parse('2009-01-01')
@@ -29,4 +39,14 @@ describe EnumDateRange do
       assert_equal(sub_ranges.count, 24)
     end
   end
+  # describe :shorten do
+  #   it "sets the last date in range" do
+  #     first = Date.parse('2009-01-01')
+  #     last = Date.parse('2011-01-01')
+  #     complete_range = first...last
+  #   
+  #     complete_range.extend EnumDateRange
+  #     complete_range = 
+  #   end
+  # end
 end
